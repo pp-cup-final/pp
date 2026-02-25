c036fac23dbbfad6f7dcb119bc820022faa514dd
pp-cup-final <dima.krutoq333@inbox.ru>
2026-02-25 16:18:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -344,7 +344,7 @@ async function updateParticipantsPP() {
         let allScores = [];
         for (let offset of [0, 100]) {
           const scoresRes = await axios.get(
-            `https://osu.ppy.sh/api/v2/users/${participant.userid}/scores/best?mode=osu&limit=100&offset=${offset}`,
+            `https://osu.ppy.sh/api/v2/users/${participant.userid}/scores/best?mode=osu&limit=25&offset=${offset}`,
             { headers: { Authorization: `Bearer ${token}` } }
           );
           allScores = allScores.concat(scoresRes.data);
